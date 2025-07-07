52fb72cd83df48990b3cb7dbddd412627c814276
Alexander McMurray <alexander.mcmurray@king.com>
2025-07-07 11:48:52 +0200

    test gh-pages upload action

---
+++ build_step.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Build script"
+
+# add the commands here
+#Install dependencies 
+npm install  
+
+#Check style
+npm run eslint
+
+# Build app
+npm run build
+
+# Run unit tests
+npm run test
+
+# Install Playwright Browsers
+npx playwright install --with-deps
+
+#Run Playwright tests
+npm run test:e2e
